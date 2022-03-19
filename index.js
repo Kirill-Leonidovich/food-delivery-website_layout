6514bd4c79145f0173e25d5cea36680e7f456c1a
Kirill Alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-03-20 01:26:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// burger
 document.querySelector('.header__burger').addEventListener('click', () => {
 	document.querySelector('.header__burger').classList.toggle('active')
 	document.querySelector('.header__menu').classList.toggle('active')
